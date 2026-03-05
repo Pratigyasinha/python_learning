cc0348bc14590f0b3564f634a681a1ec3e2aa244
Pratigya_sinha <pratigya99732@gmail.com>
2026-03-05 23:45:15 +0530

    Created prog17.py

---
+++ prog17.py
@@ -0,0 +1,16 @@
+#zeros
+a = np.zeros ((2, 3), dtype = "i")
+print ("a:\n", a)
+
+#ones
+b = np.ones ((3,3,3), dtype = "i")
+print ("\n\nb:\n", b)
+
+#arange
+#np.arange (start, stop, step)
+c = np.arange (1, 10, 2)
+print ("\n\nc:\n", c)
+
+#linspace
+d = np.linspace (1, 10, 2)
+print ("\n\nd:\n", d)
