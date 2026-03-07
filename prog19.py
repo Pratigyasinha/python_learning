c674632a401044e2aa7ccfdb92acae9ddefabb22
Pratigya_sinha <pratigya99732@gmail.com>
2026-03-07 23:19:15 +0530

    Created prog19.py

---
+++ prog19.py
@@ -0,0 +1,20 @@
+#Statistical Operations
+a = np.array ([10, 20, 30, 40, 50])
+
+#mean
+print ("a.mean():", a.mean())
+
+#median
+print ("np.median():", np.median(a))
+
+#min
+print ("a.min():", a.min())
+
+#max
+print ("a.max():", a.max())
+
+#sum
+print ("a.sum():", a.sum())
+
+#standard deviation
+print ("a.std():", a.std())
